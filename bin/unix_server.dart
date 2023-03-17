d9ec7fd88cf24620844e8401952fb90638361392
sergeilobachev <gsvlobachev@gmail.com>
2023-03-17 22:06:20 +0300

    Initial commit

---
+++ bin/unix_server.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2020, the gRPC project authors. Please see the AUTHORS file
+// for details. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// Dart implementation of the gRPC helloworld.GreetingService server.
+import 'dart:io';
+
+import 'package:grpc/grpc.dart';
+import 'package:helloworld/grpc/generated/helloworld.pbgrpc.dart';
+
+
+class GreetingServiceService extends GreetingServiceBase {
+  @override
+  Future<ResponseMessage> sayHello(ServiceCall call, RequestMessage request) async {
+    return ResponseMessage()..message = 'Hello, ${request.name}!';
+  }
+}
+
+Future<void> main(List<String> args) async {
+  final udsAddress =
+      InternetAddress('localhost', type: InternetAddressType.unix);
+  final server = Server([GreetingServiceService()]);
+  await server.serve(address: udsAddress);
+  print('Start UNIX Server @localhost...');
+}
---
+++ lib/grpc/generated/helloworld.pbjson.dart
@@ -0,0 +1,30 @@
+///
+//  Generated code. Do not modify.
+//  source: helloworld.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,deprecated_member_use_from_same_package,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use requestMessageDescriptor instead')
+const RequestMessage$json = const {
+  '1': 'RequestMessage',
+  '2': const [
+    const {'1': 'name', '3': 1, '4': 1, '5': 9, '10': 'name'},
+  ],
+};
+
+/// Descriptor for `RequestMessage`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List requestMessageDescriptor = $convert.base64Decode('Cg5SZXF1ZXN0TWVzc2FnZRISCgRuYW1lGAEgASgJUgRuYW1l');
+@$core.Deprecated('Use responseMessageDescriptor instead')
+const ResponseMessage$json = const {
+  '1': 'ResponseMessage',
+  '2': const [
+    const {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
+  ],
+};
+
+/// Descriptor for `ResponseMessage`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List responseMessageDescriptor = $convert.base64Decode('Cg9SZXNwb25zZU1lc3NhZ2USGAoHbWVzc2FnZRgBIAEoCVIHbWVzc2FnZQ==');
